1cef08dd48b36b1b38fb9978ea245165673d4957
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2024-08-11 13:16:12 -0500

    refactor: move constants to lib

--- app/_utils/Constants.tsx
+++
@@ -1,3 +0,0 @@
-export default {
-    descHero: 'Explore top travel destinations, find the best places to eat, and discover exciting events and photo spots tailored to your interests. Start planning your perfect trip today!'
-}
