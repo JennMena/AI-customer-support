59c511f02119b20ddc1dc798eab8422a20b3e24e
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2024-08-11 13:12:09 -0500

    feat: update API retrieval route

--- app/api/kb/retrieve/route.ts
+++ app/api/kb/retrieve/route.ts
@@ -1,23 +1,28 @@
+import { NextResponse } from "next/server";
 import { StringOutputParser } from "@langchain/core/output_parsers";
 import { PromptTemplate } from "@langchain/core/prompts";
 import { createVectorStore, llm } from "@/lib/ai";
-import { NextResponse } from "next/server";
+
+interface RetrieveRequest {
+  question: string;
+}
 
 export async function POST(request: Request) {
-  const { question } = await request.json();
-  const vectorStore = await createVectorStore();
-
+  const data: RetrieveRequest = await request.json();
+  const { question } = data;
+  
   // 1. Retrieve the most relevant results
+  const vectorStore = await createVectorStore();
   const retriever = vectorStore.asRetriever({
     k: 3, // Number of results to retrieve
-  });
+  }); // See https://js.langchain.com/v0.2/docs/integrations/vectorstores/pinecone/
   const results = await retriever.invoke(question);
 
   // 2. Generate an answer
-  const prompt = PromptTemplate.fromTemplate(RAG_PROMPT);
+  const promptTemplate = PromptTemplate.fromTemplate(RAG_PROMPT); // See https://js.langchain.com/v0.2/docs/concepts/#prompt-templates
   const outputParser = new StringOutputParser();
-  const chain = prompt.pipe(llm).pipe(outputParser);  // See https://js.langchain.com/v0.2/docs/how_to/sequence/
-  const completion = await chain.stream({
+  const chain = promptTemplate.pipe(llm).pipe(outputParser);  // See https://js.langchain.com/v0.2/docs/how_to/sequence/
+  const chainOutput = await chain.stream({
     context: results.map((doc, i) => `* ${doc.pageContent}`).join("\n"),
     question: question,
   });
@@ -28,7 +33,7 @@ export async function POST(request: Request) {
       const encoder = new TextEncoder() // Create a TextEncoder to convert strings to Uint8Array
       try {
         // Iterate over the streamed chunks of the response
-        for await (const chunkContent of completion) {
+        for await (const chunkContent of chainOutput) {
           if (chunkContent) {
             const text = encoder.encode(chunkContent) // Encode the content to Uint8Array
             controller.enqueue(text) // Enqueue the encoded text to the stream
